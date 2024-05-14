e3a02667b2a2f320bd9ea5173ecfd9d73c6f4780
Julio Aguilera Jr <julioaguilera337@gmail.com>
2024-05-13 20:21:58 -0500

    Created Vowels, Input and an empty arr.

---
+++ main.js
@@ -0,0 +1,7 @@
+let input = "I have to lock in!";    //My random phrase
+
+const vowels = ["a", "e", "i", "o", "u"];   //Whale only vowels
+
+let resultArray = [];   //will store the vowels from the input string
+
+//loop that goes through the Input var text
